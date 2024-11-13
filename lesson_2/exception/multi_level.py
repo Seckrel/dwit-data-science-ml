71709f72c989d6c9499d2ac44d70949f8d225f9c
seckrel <aayam.oza@gmail.com>
2024-11-13 20:07:23 +0545

    lesson2 exception handling

---
+++ lesson_2/exception/multi_level.py
@@ -0,0 +1,24 @@
+def divided(num1, num2):
+    if num2 == 0:
+        raise Exception("Cannot divide by zero")
+    return num1/num2
+    
+
+
+def main():
+    n1 = int(input("Enter First Number: "))
+    n2 = int(input("Second Number: "))
+
+    try:
+        # 1 GB file open
+        divided_val = divided(n1, n2)
+        print(f"Now we do some other stuff with {divided_val} here")
+    except Exception as e:
+        raise Exception(e)
+try:
+    main()
+except Exception as e:
+    print(e)
+
+finally:
+    print("I am finally")
